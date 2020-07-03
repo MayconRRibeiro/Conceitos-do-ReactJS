947b126eb299c1423ea9ea927be22bc6cee25b7a
Mike <m1kros@hotmail.com>
2020-07-02 23:46:49 -0300

    Adicionar repositório na API

--- src/App.js
+++ src/App.js
@@ -1,19 +1,27 @@
-import React, { useState, useEffect } from "react";
-import api from "./services/api";
+import React, { useState, useEffect } from 'react';
+import api from './services/api';
 
-import "./styles.css";
+import './styles.css';
 
 function App() {
   const [repositories, setRepositories] = useState([]);
 
   useEffect(() => {
-    api.get("repositories").then((response) => {
+    api.get('repositories').then((response) => {
       setRepositories(response.data);
     });
   }, []);
 
   async function handleAddRepository() {
-    // TODO
+    const response = await api.post('repositories', {
+      title: `App ${Date.now()}`,
+      url: 'github.com/user/novoApp',
+      techs: ['React', 'JavaScript'],
+    });
+
+    const repository = response.data;
+
+    setRepositories([...repositories, repository]);
   }
 
   async function handleRemoveRepository(id) {
